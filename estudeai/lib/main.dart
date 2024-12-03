3783a8b353eed06875fef5812931ee4048d80a54
João Madeira <joaomadeira1208@gmail.com>
2024-12-02 21:21:41 -0300

    feat: config firebase
    Configuração do firebase inicial; Realizada em um macos, pode ainda conter incompatibilidade entre versões de android e windows.

--- estudeai/lib/main.dart
+++ estudeai/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:estudeai/Views/Service/db_helper.dart';
+import 'package:estudeai/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'Views/TelaInicial/TelaInicial.dart';
@@ -8,11 +10,15 @@ import 'Views/Login/Login.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:flutter/foundation.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (!Platform.isWindows && !Platform.isLinux && !Platform.isMacOS && !Platform.isAndroid && !Platform.isIOS) {
-    throw UnsupportedError('A execução da aplicação não é suportada no navegador com sqflite.');
+  if (!Platform.isWindows &&
+      !Platform.isLinux &&
+      !Platform.isMacOS &&
+      !Platform.isAndroid &&
+      !Platform.isIOS) {
+    throw UnsupportedError(
+        'A execução da aplicação não é suportada no navegador com sqflite.');
   } else {
     // Inicializa o databaseFactory corretamente para plataformas suportadas
     if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
@@ -20,7 +26,11 @@ void main() async {
       databaseFactory = databaseFactoryFfi;
     }
   }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await DatabaseHelper.instance.database;
+  print('Firebase foi inicializado com sucesso!');
   printDatabasePath();
   runApp(const MyApp());
 }
